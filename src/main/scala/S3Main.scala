8bc8ee7269e4d7b15fef971d9a884d9113714230
cp <cnvp2003@yahoo.com>
2023-02-26 17:06:08 +0100

    Assignment commit.

--- src/main/scala/S3Main.scala
+++ src/main/scala/S3Main.scala
@@ -4,9 +4,9 @@ import com.amazonaws.services.s3.AmazonS3Client
 object S3Main extends App {
 
   val BUCKET_NAME = "testbucket"
-  val INPUT_FILE_PATH = "/Users/chandrashekhar/CP/practice/practiceScalaAkka/src/main/scala/Test.scala"
+  val INPUT_FILE_PATH = "/Users/chandrashekhar/CP/practice/practiceScalaAkka/src/main/scala/TestInput.csv"
   val INPUT_FILE_NAME = "TestInput.csv"
-  val OUTPUT_FILE_PATH = "/Users/chandrashekhar/CP/practice/practiceScalaAkka/src/main/scala/Test.scala"
+  val OUTPUT_FILE_PATH = "/Users/chandrashekhar/CP/practice/practiceScalaAkka/src/main/scala/TestOutput.tsv"
   val OUTPUT_FILE_NAME = "TestOutput.tsv"
   val AWS_ACCESS_KEY = "someaccesskey"
   val AWS_SECRET_KEY = "somesecretkey"
